b4f40506b44b01d0256ec5dc0d04cc87d0a8cf12
r4b2 <github@r4b2.de>
2021-07-17 23:37:59 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bootstrap
+
+```bash
+#!/bin/bash
+git clone https://github.com/r4b2/bootstrap.git /opt/bootstrap >> /var/log/bootstrap.log 2>&1;
+bash /opt/bootstrap/basic.sh >> /var/log/bootstrap.log 2>&1;
+```
---
+++ basic.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+HOME=/root/
+
+cd $HOME || exit
+
+apt update
+apt dist-upgrade -y
+apt install vim htop bmon jq curl
+
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+AWS_REGION=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r ".region")
+
+wget "https://s3.${AWS_REGION}.amazonaws.com/amazon-ssm-${AWS_REGION}/latest/debian_amd64/amazon-ssm-agent.deb" -O /tmp/amazon-ssm-agent.deb
+dpkg -i /tmp/amazon-ssm-agent.deb
+systemctl enable --now amazon-ssm-agent
+
+#shutdown -r now
