e28347450140965c94eecedb48ce5bf9e7d5c6d0
r4b2 <github@r4b2.de>
2021-07-18 00:19:47 +0200

    fix apt

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ```bash
 #!/bin/bash
-apt update ; apt install git
+apt update >> /var/log/bootstrap.log 2>&1;
+apt install -y git >> /var/log/bootstrap.log 2>&1;
 git clone https://github.com/r4b2/bootstrap.git /opt/bootstrap >> /var/log/bootstrap.log 2>&1;
 bash /opt/bootstrap/basic.sh >> /var/log/bootstrap.log 2>&1;
 ```
--- basic.sh
+++ basic.sh
@@ -5,7 +5,7 @@ cd $HOME || exit
 
 apt update
 apt dist-upgrade -y
-apt install vim htop bmon jq curl
+apt install -y vim htop bmon jq curl
 
 TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 AWS_REGION=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r ".region")
